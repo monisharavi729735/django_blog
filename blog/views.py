24a7f9d405db1bdb34e77830b669c4265bab979b
Monisha R <monishar729735@gmail.com>
2024-08-31 12:33:02 +0530

    db + migrations

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author':'John Smith',
-        'title' : 'Penguins',
-        'content': 'I like penguins',
-        'posted': '1st April 2023'
-    },
-    {
-        'author':'Jane Doe',
-        'title' : 'Turtles',
-        'content': 'that\'s a cute turtle',
-        'posted': '2nd April 2023'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
-    context = {'posts': posts}
+    context = {
+        'posts': Post.objects.all()
+    }
     return render(request, 'blog/home.html', context)
 
 def about(request):
