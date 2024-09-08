33fb8d4c7059dd415a5ed7cc31997a497d06e25e
Monisha R <monishar729735@gmail.com>
2024-09-08 10:32:20 +0530

    reset password

--- users/views.py
+++ users/views.py
@@ -11,12 +11,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for { username }, you can now login')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegistrationForm()
-    context = {'form': form}
-    return render(request, 'users/register.html', context)
+    return render(request, 'users/register.html', {'form': form})
+
 
 @login_required
 def profile(request):
@@ -25,19 +25,19 @@ def profile(request):
         p_form = ProfileUpdateForm(request.POST,
                                    request.FILES,
                                    instance=request.user.profile)
-        
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated')
+            messages.success(request, f'Your account has been updated!')
             return redirect('profile')
+
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
-        
+
     context = {
         'u_form': u_form,
         'p_form': p_form
     }
-    
+
     return render(request, 'users/profile.html', context)
